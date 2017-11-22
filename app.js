640c450432765cf7d15688f8e0bac93d47895293
Marcos Cavalcante <marcos.bcc2011@gmail.com>
2017-11-22 07:20:58 +0000

    Set up middleware for dependencies, create routes file, connect to mongo database

--- app.js
+++ app.js
@@ -4,16 +4,45 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const passport = require("passport");
 const mongoose = require("mongoose");
+const config   = require('./config/database');
 
+// Connect to database
+mongoose.connect( config.database );
+
+// On Connection
+mongoose.connection.on( 'connected', () => {
+    console.log( 'Connected to database ' + config.database );
+});
+
+// On Error
+mongoose.connection.on( 'error', (err) => {
+    console.log( 'Database error ' + err );
+});
 
 const app  = express();
+
+const users = require('./routes/users');
+
+// Port Number
 const port = 3000;
 
+// CORS Middleware
+app.use( cors() );
 
+// Set Static folder
+app.use( express.static( path.join( __dirname, 'public' ) ) );
+
+// BodyParser Middleware
+app.use( bodyParser.json() );
+
+app.use( '/users', users );
+
+// Index Route
 app.get('/', (req,res) => {
     res.send("Invalid endpoint");
 });
 
+// Start Server
 app.listen( port, () => {
     console.log("Server started on port " + port);
 });
